0ad39a650fb4842e8388b0be2d826f1ef77ee347
Scott Greenup <scott.j.greenup@gmail.com>
2017-05-21 18:12:34 +1000

    Basic configuration parsing, slight changes to matcher (#7)
    * Basic configuration parsing, slight changes to matcher
    
    * Fixed the makefile, whitespace

--- src/filed.py
+++ src/filed.py
@@ -1,39 +1,111 @@
 #!/usr/bin/env python
+import logging
 import re
 
 from os import walk, path, listdir
 
 
+class Rule(object):
+    src = None
+    dst = None
+    reg = None
+
+
+def parse_config_file(fd):
+    """Parses the file descriptor line by line. Skips comments. This also
+    validates all the configurations, therefore the return value should be safe
+    and usable.
+    """
+
+    rules = []
+
+    for num, line in enumerate(fd):
+        line = line.strip()
+
+        # Skip comments
+        if line.startswith('#'):
+            continue
+
+        # Format of the line is src:dst:regexpression
+        parts = line.split(':')
+        if len(parts) != 3:
+            logging.warning("{}:{}: Error reading line".format(filename, num))
+            continue
+
+        # Validate the parts of the config
+        r = Rule()
+        r.src = path.expanduser(parts[0])
+        r.dst = path.expanduser(parts[1])
+        r.reg = parts[2]
+
+        if not path.isdir(r.src):
+            logging.warning(
+                "{}:{}: {} is not a directory"
+                .format(filename, num, r.src))
+
+        if not path.isdir(r.dst):
+            logging.warning(
+                "{}:{}: {} is not a directory"
+                .format(filename, num, r.dst))
+
+        try:
+            re.compile(r.reg)
+        except Exception as e:
+            logging.warning(
+                "{}:{}: error parsing regex: {}"
+                .format(filename, num, str(e)))
+
+        rules.append(r)
+
+    return rules
+
+
+def load_config_file(filename):
+    """Loads a configuration file, and logs any troubles. Returns None if there
+    were any errors with reading or accessing the file. Returns [] if there are
+    no rules, and returns [Rules(), ...] on success.
+    """
+    filename = path.expanduser(filename)
+
+    if not path.isfile(filename):
+        logging.info("{} does not exist as a file.".format(filename))
+        return None
+
+    try:
+        with open(filename, 'r') as fd:
+            return parse_config_file(fd)
+    except IOError as e:
+        logging.warning("Could not read {}: {}", filename, str(e))
+
+    return None
+
+
 def main():
-    # TODO: Reads the config file
-    # Format: src:dest:regex
-    sources = []
-    sources.append("~/Workspace/filed/test/")
-    patterns = [".*\.pdf"]
+    config_files = [
+        '~/.filedrc'
+    ]
+
+    rules = {}
+    for filename in config_files:
+        rules[filename] = load_config_file(filename)
 
     # Monitor folder for changes
-    matches = matcher(sources, patterns)
-    print(matches)
+    for filename, rules in rules.items():
+        for rule in rules:
+            matches = matcher(rule.src, rule.reg)
+            print(matches)
 
     # TODO: Move the file if changes
 
 
 # Returns the file path that match any of the rules
-def matcher(sources, rules):
+def matcher(source, rule):
     f = []
-    for s in sources:
-        for filename in listdir(path.expanduser(s)):
-            if multimatch(filename, rules):
-                f.append(path.join(s, filename))
-        # TODO: Future subtrees - for (dirpath, dirnames, filenames) in walk(path.expanduser(s)):
+    for filename in listdir(path.expanduser(source)):
+        if re.match(rule, filename):
+            f.append(path.join(source, filename))
+    # TODO: Future subtrees - for (dirpath, dirnames, filenames) in walk(path.expanduser(s)):
     return f
-
-
-# Inefficient way to attempt to match against multiple patterns
-def multimatch(word, patterns):
-    for pattern in patterns:
-        if re.search(pattern, word):
-            return True
 
 if __name__ == "__main__":
     main()
